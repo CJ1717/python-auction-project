956807994c18ccb72454648f2178fb18f78bdf2e
CJ1717 <camjones0505@gmail.com>
2025-04-07 20:20:32 -0400

    fixed code

--- auction project.py
+++ auction project.py
@@ -1,6 +1,4 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>>starting_bid= int(input("What would you like the starting bid to be?"))
+starting_bid= int(input("What would you like the starting bid to be?"))
 bid=int(input("What would you like to bid?"))
 while True:
     if (int(bid))>(int(starting_bid)):
@@ -9,4 +7,4 @@ while True:
     else: 
         bid=input("Bid is too low what would you like to bid?")
 print ("Congrats you have won the auction payment is expected in 24 hours!")
-... 
+
