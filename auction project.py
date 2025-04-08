2bee35da7443093b7e7a25ef914bce4cc149bfb1
Jacob Cook <61634762+jcook135@users.noreply.github.com>
2025-04-07 20:24:09 -0400

    Fix spacing

--- auction project.py
+++ auction project.py
@@ -1,10 +1,9 @@
-starting_bid= int(input("What would you like the starting bid to be?"))
-bid=int(input("What would you like to bid?"))
+starting_bid = int(input("What would you like the starting bid to be?"))
+bid = int(input("What would you like to bid?"))
 while True:
-    if (int(bid))>(int(starting_bid)):
+    if (int(bid)) > (int(starting_bid)):
         print("You have won the auction")
         break
     else: 
-        bid=input("Bid is too low what would you like to bid?")
-print ("Congrats you have won the auction payment is expected in 24 hours!")
-
+        bid = input("Bid is too low what would you like to bid?")
+print("Congrats you have won the auction payment is expected in 24 hours!")
